c1ab3ec189fb9758c9638960d54adbc7807435dd
nguyenanhky <chienky2016@gmail.com>
2024-03-17 13:51:26 +0700

    TopAppBar

--- app/src/main/java/kynv1/fsoft/jetpack_compose_livescoreapp_mvvm/MainActivity.kt
+++ app/src/main/java/kynv1/fsoft/jetpack_compose_livescoreapp_mvvm/MainActivity.kt
@@ -3,13 +3,26 @@ package kynv1.fsoft.jetpack_compose_livescoreapp_mvvm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kynv1.fsoft.jetpack_compose_livescoreapp_mvvm.ui.theme.JetpackComposeLiveScoreAppMVVMTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +33,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,17 +43,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun LiveScore(
+    modifier: Modifier = Modifier,
+) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(10.dp)
+    ) {
+        TopAppBar()
+    }
+}
+
+@Composable
+fun TopAppBar(
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Refresh, contentDescription = null)
+        }
+        Text(text = "LiveScores", style = MaterialTheme.typography.h4)
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.modeicon),
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopAppbarPreview() {
+    TopAppBar()
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LiveScorePreview() {
     JetpackComposeLiveScoreAppMVVMTheme {
-        Greeting("Android")
+        LiveScore()
     }
 }
